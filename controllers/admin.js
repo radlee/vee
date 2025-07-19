aebcc5777aa05af5b78a217ebb4a7b059bd7539a
radlee <prof.lolito@gmail.com>
2025-07-19 21:43:33 +0200

    Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,9 +11,15 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect('/');
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    
+    const product = new Product(title, imageUrl, price, description);
+    
+    product.save();
+    res.redirect('/');
 };
 
 exports.getProducts = (req, res, next)=> {
